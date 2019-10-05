cbd64b4c19d877b4cc782d2989ba943f608a09f7
ElenaPopova18 <56185573+ElenaPopova18@users.noreply.github.com>
2019-10-05 11:11:12 +0300

    Create Ex3.lisp

---
+++ Ex3.lisp
@@ -0,0 +1,4 @@
+(nth 2 (list (list 5 6 7) 6 (list 2 3 4) (car(list 1 2 3)) (list 13 14 15)));; 3ий элемент из списка
+(nth 1 (nth 4 (list (list 5 6 7) 6 (list 2 3 4) (car(list 1 2 3)) (list 13 14 15))));; 2ой элемент и 5ый из списка
+(+ (nth 1 (list (list 5 6 7) 6 (list 2 3 4) (car(list 1 2 3)) (list 13 14 15))) (nth 3 (list (list 5 6 7) 6 (list 2 3 4) (car(list 1 2 3)) (list 13 14 15))));; Сумма
+(nth 2 (nth 0 (list (list 5 6 7) 6 (list 2 3 4) (car(list 1 2 3)) (list 13 14 15))));; 3ий элемент 1ого списка
